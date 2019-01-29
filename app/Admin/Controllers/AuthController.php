f8cdf747dcd66ce61849f7a57dd25629e09dc683
chinwe.jing <1207511201@qq.com>
2019-01-29 18:42:00 +0800

    新增禁用管理员功能

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -3,8 +3,39 @@
 namespace App\Admin\Controllers;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class AuthController extends BaseAuthController
 {
+    /**
+     * Handle a login request.
+     *
+     * @param Request $request
+     *
+     * @return mixed
+     */
+    public function postLogin(Request $request)
+    {
+        $credentials = $request->only([$this->username(), 'password']);
 
+        /** @var \Illuminate\Validation\Validator $validator */
+        $validator = Validator::make($credentials, [
+            // admin_users表种要存在username字段，且status必须为1(开启)
+            $this->username()   => 'required|string|exists:admin_users,username,status,1',
+            'password'          => 'required|string|',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withInput()->withErrors($validator);
+        }
+
+        if ($this->guard()->attempt($credentials)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        return back()->withInput()->withErrors([
+            $this->username() => $this->getFailedLoginMessage(),
+        ]);
+    }
 }
