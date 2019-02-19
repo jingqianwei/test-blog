ac1e84d8de8ba0ebe486e96bbf205ea290f31425
chinwe.jing <1207511201@qq.com>
2019-02-19 14:08:13 +0800

    composer ide 提示插件

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //更改日期提示为中文
+        Carbon::setLocale('zh');
+
+        //设置数据表字符串字段的默认长度
         Schema::defaultStringLength(191);
     }
 
@@ -24,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app->environment() !== 'production') { //phpstorm 提示插件,本地开发才生效
+            //参考网址：https://github.com/barryvdh/laravel-ide-helper
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 }
