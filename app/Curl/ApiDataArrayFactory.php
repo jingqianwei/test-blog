7f13d850e45b7dea9255a4f7308574921711ae36
chinwe.jing <1207511201@qq.com>
2019-02-27 16:38:51 +0800

    fix

---
+++ app/Curl/ApiDataArrayFactory.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chinwe.jing
+ * Date: 2019/2/27
+ * Time: 16:06
+ */
+
+namespace App\Curl;
+
+
+use App\Exceptions\ResponseNotJsonException;
+use App\Exceptions\ResponseNotXMLException;
+use SimpleXMLElement;
+
+class ApiDataArrayFactory
+{
+    public static function make(CurlInterfaceDriver $curl)
+    {
+        if ($curl instanceof JsonHttpCurlDriver) {
+            return static::json2Array($curl->getResponse());
+        }
+
+        if ($curl instanceof XMLHttpCurlDriver) {
+            try {
+                return static::xml2Array($curl->getResponse());
+            } catch (\Exception $e) {
+                throw new $e;
+            }
+        }
+    }
+
+    /**
+     * json转数组
+     * @param $json
+     * @return mixed
+     * @throws ResponseNotJsonException
+     */
+    private static function json2Array($json)
+    {
+        try {
+            $data = \GuzzleHttp\json_decode($json, true);
+        } catch (\InvalidArgumentException $e) {
+            throw new ResponseNotJsonException($e->getMessage(), $e->getCode());
+        }
+
+        return $data;
+    }
+
+    /**
+     * xml转数组
+     * @param $xml
+     * @return array
+     * @throws ResponseNotXMLException
+     */
+    private static function xml2Array($xml)
+    {
+        if (!static::isXml($xml)) {
+            throw new ResponseNotXMLException('传的值不是xml', 201);
+        }
+
+        $xml = new SimpleXMLElement($xml);
+
+        return self::_XML2Array($xml);
+    }
+
+    /**
+     * xml数据处理
+     * @param SimpleXMLElement $parent
+     * @return array
+     */
+    private static function _XML2Array(SimpleXMLElement $parent)
+    {
+        $array = array();
+
+        foreach ($parent as $name => $element) {
+            ($node = &$array[$name])
+            && (1 === count($node) ? $node = array($node) : 1)
+            && $node = &$node[];
+
+            $node = $element->count() ? self::_XML2Array($element) : trim($element);
+        }
+
+        return $array;
+    }
+
+    /**
+     * 判断是否为xml
+     * @param $xml
+     * @return int
+     */
+    private static function isXml($xml)
+    {
+        return xml_parse(xml_parser_create(), $xml, true);
+    }
+}
