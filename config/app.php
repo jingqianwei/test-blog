fb0765617e448bf6cfdf8a88f5571cd63930dfdf
chinwe.jing <1207511201@qq.com>
2019-03-08 18:20:38 +0800

    服务提供器

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        // 新增的服务
+        // 新增的服务提供器
         App\Providers\RickServiceProvider::class,
 
     ],
