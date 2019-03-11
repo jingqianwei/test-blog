751f33673a897f142ed4083d001cb130048fddab
chinwe.jing <1207511201@qq.com>
2019-03-11 11:23:12 +0800

    fix 计划任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,10 +28,15 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
 
         // 每天凌晨1点清理一下项目所有缓存
-        $schedule->command('clear:project-cache')->dailyAt('01:00');
+        //$schedule->command('clear:project-cache')->dailyAt('01:00');
+        $schedule->command('clear:project-cache')->everyMinute();
 
         // 每周执行一次数据库备份
-        $schedule->command('db:backup')->weekly();
+        //$schedule->command('db:backup')->weekly();
+        $schedule->command('db:backup')->everyMinute();
+
+        // 每分钟测试
+        $schedule->command('test:command')->everyMinute();
     }
 
     /**
