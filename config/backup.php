85df24e8e80b16fd2c6640559aee11923b26b32d
chinwe.jing <1207511201@qq.com>
2019-03-11 16:22:18 +0800

    数据备份

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => strtolower(config('app.name')),
+        'name' => config('app.name'),
 
         'source' => [
 
