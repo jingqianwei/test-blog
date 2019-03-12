e1e36f99488926470c97067ffcad07c2ff11d36a
chinwe.jing <1207511201@qq.com>
2019-03-12 12:02:57 +0800

    增加帖子浏览量功能

--- app/Listeners/PostEventListener.php
+++ app/Listeners/PostEventListener.php
@@ -4,8 +4,6 @@ namespace App\Listeners;
 
 use App\Events\PostViewEvent;
 use App\Models\Post;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Redis;
 
 class PostEventListener
