67e29987b81a1fa1b73904e677a42b2736a9ddfa
chinwe.jing <1207511201@qq.com>
2019-03-29 13:50:48 +0800

    '测试rebase的使用'

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -115,4 +115,9 @@ class PostController extends Controller
         Redis::set('test_type', 44444444444444); // 不过期
         Redis::setex('user_type_' . $id, 3600, $request->name);
     }
+
+    public function testRebase()
+    {
+        return 'rebase';
+    }
 }
