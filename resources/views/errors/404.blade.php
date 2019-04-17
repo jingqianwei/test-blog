9524109fe989801008cc8db99a9fa8bb77618ec5
chinwe.jing <1207511201@qq.com>
2019-04-17 18:53:52 +0800

    vuejs练习

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -461,13 +461,13 @@
     <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
         <div class="max-w-sm m-8">
             <div class="text-black text-5xl md:text-15xl font-black">
-                {{ $code }}
+                {{ $code ?? 404 }}
             </div>
 
             <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
 
             <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
-                {{ $msg }}
+                {{ $msg ?? '' }}
             </p>
 
             <a href="{{ url('/') }}">
--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -2,10 +2,11 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <link rel="icon" href="/images/touch-icon.png" />
+    <title>追风少年的博客</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="{{ mix('css/blog.css') }}">
 </head>
