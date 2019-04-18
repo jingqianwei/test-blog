929d1ce4d401c1a8989505a741957aabab3a689e
chinwe.jing <1207511201@qq.com>
2019-04-18 17:06:43 +0800

    test websocket

--- app/Console/Commands/PracticeWebSocket.php
+++ app/Console/Commands/PracticeWebSocket.php
@@ -59,6 +59,7 @@ class PracticeWebSocket extends Command
             // token验证推送来源，避免恶意访问
             // 接收http请求从post获取message参数的值，给用户推送
             // $this->webSocket->connections 遍历所有WebSocket连接用户的fd，给所有用户推送
+            var_dump($request->post);
             foreach ($this->webSocket->connections as $fd) {
                 $this->webSocket->push($fd, $request->post['info']);
             }
