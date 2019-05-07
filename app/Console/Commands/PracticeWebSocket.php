699bf4ca8b4e44ea96f4e964ae8554a99c4ab908
chinwe.jing <1207511201@qq.com>
2019-05-07 14:37:48 +0800

    test websocket

--- app/Console/Commands/PracticeWebSocket.php
+++ app/Console/Commands/PracticeWebSocket.php
@@ -75,12 +75,10 @@ class PracticeWebSocket extends Command
     protected function  onRecordComment(\swoole_websocket_server $ws, $frame)
     {
         $data = json_decode($frame->data,true);
-        Log::info('连接的fd数为 start：' . count($ws->connections));
         Log::info('websocket接受到的数据为', $data);
-        Log::info('连接的fd数为 end ' . count($ws->connections));
         foreach ($ws->connections as $fd){
             Log::info('连接的fd为' . $fd);
-            $ws->push($fd, json_encode(array('errcode'=>'0','errmsg'=>'成功','data'=>$data),JSON_UNESCAPED_UNICODE));
+            $ws->push($fd, json_encode(['errcode'=>'0', 'errmsg'=>'成功', 'data'=>$data],JSON_UNESCAPED_UNICODE));
         }
 
         return true;
