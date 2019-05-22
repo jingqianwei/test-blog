7e04c737f32bc3f3114be90d0d12e4405ba77403
chinwe.jing <1207511201@qq.com>
2019-05-22 19:31:46 +0800

    composer update

--- app/Console/Commands/ClearProjectCache.php
+++ app/Console/Commands/ClearProjectCache.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 class ClearProjectCache extends Command
 {
@@ -40,34 +42,40 @@ class ClearProjectCache extends Command
         //获取传入的参数
         $clearContent = $this->argument('clear');
 
+        // 进度条
+        $bar = $this->output->createProgressBar();
+
+        // 开始
+        $bar->start();
+
         switch ($clearContent) {
             case 'route':
-                \Artisan::call('route:clear');
+                Artisan::call('route:clear');
                 $this->info('路由缓存清理成功！');
                 break;
             case 'view':
-                \Artisan::call('view:clear');
+                Artisan::call('view:clear');
                 $this->info('视图缓存清理成功！');
                 break;
             case 'config':
-                \Artisan::call('config:clear');
+                Artisan::call('config:clear');
                 $this->info('配置缓存清理成功！');
                 break;
             case 'cache':
-                \Artisan::call('cache:clear');
+                Artisan::call('cache:clear');
                 $this->info('缓存内容清理成功！');
                 break;
             case 'opcache':
-                \Artisan::call('opcache:clear');
+                Artisan::call('opcache:clear');
                 $this->info('php中opcache缓存清理成功！');
                 break;
             case 'all':
-                \Artisan::call('route:clear');
-                \Artisan::call('view:clear');
-                \Artisan::call('config:clear');
-                \Artisan::call('cache:clear');
-                \Artisan::call('optimize:clear');
-                \Artisan::call('opcache:clear');
+                Artisan::call('route:clear');
+                Artisan::call('view:clear');
+                Artisan::call('config:clear');
+                Artisan::call('cache:clear');
+                Artisan::call('optimize:clear');
+                Artisan::call('opcache:clear');
                 $this->info('项目缓存清理成功！');
                 break;
             default:
@@ -75,7 +83,10 @@ class ClearProjectCache extends Command
                 break;
         }
 
-        \Log::info('数据清理成功');
+        // 完成
+        $bar->finish();
+
+        Log::info('数据清理成功');
         return true;
     }
 }
