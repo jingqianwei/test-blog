6b49777ef5d59db4b9e020e2c12a45204dd45aa5
chinwe.jing <1207511201@qq.com>
2019-05-29 15:09:59 +0800

    练习单元测试的使用

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,18 +2,24 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
     /**
+     * windows上使用方法 C:\phpStudy\WWW\test-blog\vendor\bin\phpunit --filter testBasicTest
+     *
      * A basic test example.
      *
      * @return void
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        $res = User::find(1);
+        $this->assertTrue((bool)$res); // 判断运行结果是否为true
+        $name = '景乾威888';
+        $this->assertEquals($name, $res->name); // 判断两个变量是否相等
     }
 }
