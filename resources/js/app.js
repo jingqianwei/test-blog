ff21ce73d4c915ee5019d8f8d1739ca49c84e431
chinwe.jing <1207511201@qq.com>
2019-09-09 10:47:25 +0800

    博客开发

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-//require('./bootstrap');
 window.Vue = require('vue');
 import router from './router'
+import store from './store'
 //import elementUI from 'element-ui';
 //import 'element-ui/lib/theme-chalk/index.css';
 //Vue.use(elementUI, {size: 'medium'}); // 将elementUI引入vue中
@@ -9,5 +9,6 @@ import App from './App.vue';
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 });
